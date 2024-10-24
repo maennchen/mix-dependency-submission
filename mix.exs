23150238040614ff9c4813ec88443aaa52ffc93d
Jonatan Männchen <jonatan@maennchen.ch>
2024-10-24 21:22:55 +0000

    Release v1.0.0-beta.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@
 defmodule MixDependencySubmission.MixProject do
   use Mix.Project
 
-  @version "1.0.0-beta.1"
+  @version "1.0.0-beta.2"
   @source_url "https://github.com/maennchen/mix-dependency-submission"
   @description """
   :warning: This repository is not ready for use. Please check back later.
